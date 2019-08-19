9c91f9eaac7272a06d6b5d3fef89b78e3576eb96
Nagarjuna <w3.nagarjuna@gmail.com>
2019-08-19 11:26:05 +0530

    typescript changes

--- typescript/temp.ts
+++
@@ -1,9 +0,0 @@
-function padLeft(value: string, padding: string | number) {
-    if (typeof padding === "number") {
-        // Do something and return
-    }
-    if (typeof padding === "string") {
-        // Do something and return
-    }
-    throw new Error(`Expected string or number, got '${padding}'.`);
-}
